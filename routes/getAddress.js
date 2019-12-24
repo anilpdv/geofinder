1c1c9ba06a06d3b44ddb60b0a7e2e5e8c61fc5d5
shoebhashcorp <shoeb.shaik@msr-it.com>
2019-12-24 14:06:50 +0530

    changing mistakes

--- routes/getAddress.js
+++ routes/getAddress.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const geoip = require("geoip-lite");
 
 router.get("/", (req, res) => {
-  const ip =
-    request.headers["x-forwarded-for"] || request.connection.remoteAddress;
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   console.log({ ip });
   const geo = geoip.lookup(ip);
   res.json(geo);
