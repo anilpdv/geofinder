4b723595b22617006792469e74455b6d2b5ae941
shoebhashcorp <shoeb.shaik@msr-it.com>
2019-12-24 16:57:14 +0530

    changing the data format

--- routes/getAddress.js
+++ routes/getAddress.js
@@ -4,12 +4,45 @@ const geoip = require("geoip-lite");
 const countryjs = require("countryjs");
 
 router.get("/", async (req, res) => {
+  const data = {};
   const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
   console.log({ ip });
   const geo = geoip.lookup(ip);
-  const countryData = countryjs.info(geo.country);
+  // : info
+  data.ip = ip;
+  data.country_code = geo.country;
+  data.region_code = geo.region;
+  data.city = geo.city;
 
-  res.json({ info: geo, country: countryData });
+  const countryData = countryjs.info(geo.country);
+  data.country_name = countryData.name;
+  data.latitude = geo.ll[0];
+  data.longitude = geo.ll[1];
+  data.continent_name = countryData.region;
+
+  // : location details
+  data.location = {};
+  data.location.capital = countryData.capital;
+  data.location.country_flag = `http://assets.ipstack.com/flags/${countryData.ISO.alpha2.toLowerCase()}.svg`;
+  data.location.calling_codes = countryData.callingCodes;
+  data.location.population = countryData.population;
+  data.location.languages = countryData.languages;
+  data.location.states_no = countryData.provinces.length;
+  data.location.states = countryData.provinces;
+  data.location.native_name = countryData.nativeName;
+  data.location.is_eu = geo.eu;
+
+  // : time zone details
+  data.time_zone = {};
+  data.time_zone.id = geo.timezone;
+  data.current_time = countryData.timezones[0];
+
+  // : currency
+  data.currency = {};
+  data.currency.code = countryData.currencies;
+
+  res.json(data);
 });
 
 module.exports = router;
--- utils/getGeoData.js
+++ utils/getGeoData.js
@@ -1,4 +1,6 @@
 const country = require("countryjs");
+const numbertowords = require("number-to-words");
 
-const data = country.info("IN");
-console.dir(data);
+const data = country.info("US");
+const words = numbertowords.toWordsOrdinal(data.population);
+console.dir(words);
